024109c97268bd5af4adeb3c3dc4eccb53d03442
Prashant Gaikwad <prashant.gaikwad@leftrightmind.com>
2019-10-04 10:32:20 +0530

    BarView and Dark mode support added

--- BarChart SwiftUI/ContentView.swift
+++ BarChart SwiftUI/ContentView.swift
@@ -9,13 +9,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var pickerSelectedItem: Int = 0
+    @State var dataPoints: [[CGFloat]] = [
+        [140,80,180,70,140],
+        [120,70,140,80,180],
+        [170,60,110,180,70]
+    ]
+    
     var body: some View {
-        Text("My Score")
+        ZStack {
+            Color("appBackground").edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("My Chart")
+                .foregroundColor(Color("title"))
+                    .font(.system(size:34))
+                    .fontWeight(.heavy)
+                
+                Picker(selection: $pickerSelectedItem, label: Text("")) {
+                    Text("Prashant").tag(0)
+                    Text("Aman").tag(1)
+                    Text("Payal").tag(2)
+                }
+                .font(.system(size:20))
+                .pickerStyle(SegmentedPickerStyle())
+                .padding(.horizontal,24)
+                
+                HStack(spacing:30) {
+                    BarChart(barValue: dataPoints[pickerSelectedItem][0])
+                    BarChart(barValue: dataPoints[pickerSelectedItem][1])
+                    BarChart(barValue: dataPoints[pickerSelectedItem][2])
+                    BarChart(barValue: dataPoints[pickerSelectedItem][3])
+                    BarChart(barValue: dataPoints[pickerSelectedItem][4])
+                }
+                .padding(.top,24)
+                .animation(.default)
+                
+                
+            }
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+struct BarChart: View {
+    
+    var barValue: CGFloat = 0
+    
+    var body : some View {
+        VStack {
+            ZStack(alignment: .bottom) {
+                Capsule().frame(width: 30, height: 200)
+                    .foregroundColor(Color(#colorLiteral(red: 0.3936573267, green: 0.8918792605, blue: 0.7495730519, alpha: 1)))
+                Capsule().frame(width: 30, height: barValue)
+                    .foregroundColor(.white)
+            }
+            
+            
+            Text("D")
+                .padding(.top,8)
+        }
     }
 }
