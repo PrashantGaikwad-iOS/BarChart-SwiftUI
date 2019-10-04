ca480dbba099fbf1c1e93023d052b9152ce8c6ea
Prashant Gaikwad <prashant.gaikwad@leftrightmind.com>
2019-10-04 09:49:41 +0530

    Initial Commit

---
+++ BarChart SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BarChart SwiftUI
+//
+//  Created by Saif on 04/10/19.
+//  Copyright © 2019 LeftRightMind. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("My Score")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
